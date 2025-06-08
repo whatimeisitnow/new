62b4dd41eee285655cca323fcd47c63d0487436b
whatimeisitnow <jayjay.hi.77@gmail.com>
2025-06-08 17:12:43 +0900

    0608

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = 'modelproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('DB_NAME'), # DB(스키마) 이름
+#         'USER': config('DB_USER'), # 유저 이름 (root)
+#         'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
+#         'HOST': config('DB_HOST'), # DB 엔드포인트
+#         'PORT': 3306,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'), # DB(스키마) 이름
-        'USER': config('DB_USER'), # 유저 이름 (root)
-        'PASSWORD': config('DB_PASSWORD'), # DB 비밀번호
-        'HOST': config('DB_HOST'), # DB 엔드포인트
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
